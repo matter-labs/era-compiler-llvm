ed34099d955ee423a6750e537e6437c1fee1c0ad
Alex Z <a.zarudnyy@matterlabs.dev>
2021-03-25 00:05:22 +0200

    Update the build script

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ cmake \
     -S 'llvm' \
     -B 'build/' \
     -G 'Unix Makefiles' \
-    -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm" \
+    -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm/" \
+    -DCMAKE_BUILD_TYPE='Debug' \
     -DLLVM_TARGETS_TO_BUILD='' \
     -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
+    -DLLVM_ENABLE_ASSERTIONS='On' \
+    -DLLVM_OPTIMIZED_TABLEGEN='On' \
     -DCMAKE_C_COMPILER='clang-11' \
     -DCMAKE_CXX_COMPILER='clang++-11' \
-    -DLLVM_USE_LINKER='lld-11' \
-    -DCMAKE_BUILD_TYPE='Debug' \
-    -DLLVM_ENABLE_ASSERTIONS='On' \
-    -DBUILD_SHARED_LIBS='On' \
-    -DLLVM_OPTIMIZED_TABLEGEN='On'
+    -DLLVM_USE_LINKER='lld-11'
+
 cd 'build/'
-make -j8
+make -j "$(nproc)"
 make install
