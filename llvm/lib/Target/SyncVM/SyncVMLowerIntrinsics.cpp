8b07334e2aac58d2cb0569e4bb948038ed462aff
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-04-09 11:32:12 +0200

    Support memcpy, memset, memmov
    The intrinsics are lowered by LowerMemIntrinsics utility pass.

---
+++ llvm/lib/Target/SyncVM/SyncVMLowerIntrinsics.cpp
@@ -0,0 +1,113 @@
+//===-- SyncVMLowerIntrinsics.cpp -----------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "SyncVM.h"
+#include "SyncVMSubtarget.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Transforms/Utils/LowerMemIntrinsics.h"
+
+#define DEBUG_TYPE "syncvm-lower-intrinsics"
+
+using namespace llvm;
+
+namespace {
+
+class SyncVMLowerIntrinsics : public ModulePass {
+public:
+  static char ID;
+
+  SyncVMLowerIntrinsics() : ModulePass(ID) {}
+
+  bool runOnModule(Module &M) override;
+  bool expandMemIntrinsicUses(Function &F);
+  StringRef getPassName() const override { return "SyncVM Lower Intrinsics"; }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<TargetTransformInfoWrapperPass>();
+  }
+};
+
+} // namespace
+
+char SyncVMLowerIntrinsics::ID = 0;
+
+INITIALIZE_PASS(SyncVMLowerIntrinsics, DEBUG_TYPE, "Lower intrinsics", false,
+                false)
+
+bool SyncVMLowerIntrinsics::expandMemIntrinsicUses(Function &F) {
+  Intrinsic::ID ID = F.getIntrinsicID();
+  bool Changed = false;
+
+  for (auto I = F.user_begin(), E = F.user_end(); I != E;) {
+    Instruction *Inst = cast<Instruction>(*I);
+    ++I;
+
+    switch (ID) {
+    case Intrinsic::memcpy: {
+      auto *Memcpy = cast<MemCpyInst>(Inst);
+      Function *ParentFunc = Memcpy->getParent()->getParent();
+      const TargetTransformInfo &TTI =
+          getAnalysis<TargetTransformInfoWrapperPass>().getTTI(*ParentFunc);
+      expandMemCpyAsLoop(Memcpy, TTI);
+      Changed = true;
+      Memcpy->eraseFromParent();
+
+      break;
+    }
+    case Intrinsic::memmove: {
+      auto *Memmove = cast<MemMoveInst>(Inst);
+      expandMemMoveAsLoop(Memmove);
+      Changed = true;
+      Memmove->eraseFromParent();
+
+      break;
+    }
+    case Intrinsic::memset: {
+      auto *Memset = cast<MemSetInst>(Inst);
+      expandMemSetAsLoop(Memset);
+      Changed = true;
+      Memset->eraseFromParent();
+
+      break;
+    }
+    default:
+      break;
+    }
+  }
+
+  return Changed;
+}
+
+bool SyncVMLowerIntrinsics::runOnModule(Module &M) {
+  bool Changed = false;
+
+  for (Function &F : M) {
+    if (!F.isDeclaration())
+      continue;
+
+    switch (F.getIntrinsicID()) {
+    case Intrinsic::memcpy:
+    case Intrinsic::memmove:
+    case Intrinsic::memset:
+      if (expandMemIntrinsicUses(F))
+        Changed = true;
+      break;
+
+    default:
+      break;
+    }
+  }
+
+  return Changed;
+}
+
+ModulePass *llvm::createSyncVMLowerIntrinsicsPass() {
+  return new SyncVMLowerIntrinsics();
+}
--- llvm/test/CodeGen/SyncVM/intrinsic.ll
+++ llvm/test/CodeGen/SyncVM/intrinsic.ll
@@ -24,6 +24,28 @@ define i256 @hout() nounwind {
   ret i256 %1
 }
 
+; CHECK-LABEL: memcpy00
+define void @memcpy00(i256* %dest, i256* %src, i256 %size) {
+  call void @llvm.memcpy.p0i256.p0i256.i256(i256* %dest, i256* %src, i256 %size, i1 false)
+  ret void
+}
+
+; CHECK-LABEL: memmov00
+define void @memmov00(i256* %dest, i256* %src, i256 %size) {
+  call void @llvm.memmov.p0i256.p0i256.i256(i256* %dest, i256* %src, i256 %size, i1 false)
+  ret void
+}
+
+; TODO: memset require val to be i8. Needs to be fixed.
+; CHECK-LABEL: memset0
+define void @memset0(i256* %dest, i8 %val, i256 %size) {
+  call void @llvm.memset.p0i256.i256(i256* %dest, i8 %val, i256 %size, i1 false)
+  ret void
+}
+
 declare void @llvm.syncvm.habs(i256 %in)
 declare void @llvm.syncvm.habsr(i256 %in)
 declare i256 @llvm.syncvm.hout()
+declare void @llvm.memcpy.p0i256.p0i256.i256(i256*, i256*, i256, i1)
+declare void @llvm.memmov.p0i256.p0i256.i256(i256*, i256*, i256, i1)
+declare void @llvm.memset.p0i256.i256(i256*, i8, i256, i1)
