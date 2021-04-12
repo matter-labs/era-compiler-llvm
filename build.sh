a9f45ef083afcab2e773c4801627c9016dfcf8b7
Alex Z <a.zarudnyy@matterlabs.dev>
2021-04-12 21:59:55 +0300

    Add the build type to the build script

--- build.sh
+++ build.sh
@@ -2,12 +2,28 @@
 
 set -Cex
 
+# Target build: 'debug' | 'release'
+case "${1}" in
+    debug)
+        export DIRECTORY_SUFFIX="debug"
+        export BUILD_TYPE="Debug"
+        ;;
+    release)
+        export DIRECTORY_SUFFIX="release"
+        export BUILD_TYPE="Release"
+        ;;
+    *)
+        export DIRECTORY_SUFFIX="release"
+        export BUILD_TYPE="Release"
+        ;;
+esac
+
 cmake \
     -S 'llvm' \
     -B 'build/' \
     -G 'Unix Makefiles' \
-    -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm/" \
-    -DCMAKE_BUILD_TYPE='Debug' \
+    -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
+    -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
     -DLLVM_TARGETS_TO_BUILD='X86' \
     -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
     -DLLVM_ENABLE_ASSERTIONS='On' \
