f2524bf6a9722855ec8dfc5b2c80ba5c90749b91
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-04-29 00:17:26 +0200

    Register syncvm target
    Opt and bugpoint now understand target triple = "syncvm"

--- llvm/include/llvm/ADT/Triple.h
+++ llvm/include/llvm/ADT/Triple.h
@@ -100,6 +100,9 @@ public:
     wasm64,         // WebAssembly with 64-bit pointers
     renderscript32, // 32-bit RenderScript
     renderscript64, // 64-bit RenderScript
+    // SyncVM local begin
+    syncvm,         // SyncVM: syncvm
+    // SyncVM local end
     ve,             // NEC SX-Aurora Vector Engine
     LastArchType = ve
   };
