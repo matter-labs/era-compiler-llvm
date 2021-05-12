3e04ba85bc0a840835e8ec24ccd138c459beb446
Alex Z <a.zarudnyy@matterlabs.dev>
2021-05-12 11:46:48 +0300

    Update the build script, disable docs

--- build.sh
+++ build.sh
@@ -29,14 +29,20 @@ cmake \
     -G 'Unix Makefiles' \
     -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
     -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+    -DCMAKE_C_COMPILER='clang-11' \
+    -DCMAKE_CXX_COMPILER='clang++-11' \
     -DLLVM_TARGETS_TO_BUILD='X86' \
     -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
     -DLLVM_ENABLE_ASSERTIONS='On' \
     -DLLVM_OPTIMIZED_TABLEGEN='On' \
-    -DCMAKE_C_COMPILER='clang-11' \
-    -DCMAKE_CXX_COMPILER='clang++-11' \
-    -DLLVM_USE_LINKER='lld-11'
+    -DLLVM_USE_LINKER='lld-11' \
+    -DLLVM_BUILD_DOCS='Off' \
+    -DLLVM_INCLUDE_DOCS='Off' \
+    -DLLVM_ENABLE_DOXYGEN='Off' \
+    -DLLVM_ENABLE_SPHINX='Off' \
+    -DLLVM_ENABLE_OCAMLDOC='Off' \
+    -DLLVM_ENABLE_BINDINGS='Off'
 
 cd "build-${DIRECTORY_SUFFIX}/"
 make -j "$(nproc)"
-make install
+sudo make install
