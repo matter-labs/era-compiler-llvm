7ef372e6eeb9d165dd5a1efbea4e6e82f4897ddf
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-05-14 00:15:02 +0200

    Lower CopyToReg rN, FI to load
    Transform CopyToReg from the frame to load from it, thus make it
    selected as MOVsr.

--- llvm/lib/Target/SyncVM/SyncVMISelLowering.h
+++ llvm/lib/Target/SyncVM/SyncVMISelLowering.h
@@ -51,6 +51,8 @@ public:
   SDValue LowerSELECT_CC(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerAnd(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerShl(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerFrameIndex(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerCopyToReg(SDValue Op, SelectionDAG &DAG) const;
 
   SDValue LowerBrccBr(SDValue Op, SDValue DestFalse, SDLoc DL, SelectionDAG &DAG) const;
   SDValue LowerBrcondBr(SDValue Op, SDValue DestFalse, SDLoc DL,
---
+++ llvm/test/CodeGen/SyncVM/copytoreg_bug.ll
@@ -0,0 +1,17 @@
+; RUN: llc -O0 < %s | FileCheck %s
+
+target datalayout = "e-p:256:256-i256:256:256"
+target triple = "syncvm"
+
+; CHECK-LABEL: foo
+define dso_local void @foo() {
+entry:
+  %0 = alloca i8, align 32
+  %1 = bitcast i8* %0 to i256*
+; CHECK: mov 0(sp), r1
+  br label %condition
+
+condition:                                        ; preds = %condition, %entry
+  %2 = getelementptr i256, i256* %1, i256 undef
+  br label %condition
+}
