5708e0bb8246cb2428bf94df74f3d61b443719e0
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-05-25 09:32:05 +0200

    Skip MemCpy Optimizer for SyncVM

--- llvm/lib/Transforms/Scalar/MemCpyOptimizer.cpp
+++ llvm/lib/Transforms/Scalar/MemCpyOptimizer.cpp
@@ -1791,7 +1791,10 @@ bool MemCpyOptPass::runImpl(Function &F, MemoryDependenceResults *MD_,
 
 /// This is the main transformation entry point for a function.
 bool MemCpyOptLegacyPass::runOnFunction(Function &F) {
-  if (skipFunction(F))
+  // TODO: The pass should only be skipped for SyncVM.
+  // SyncVM local begin
+  if (true || skipFunction(F))
+  // SyncVM local end
     return false;
 
   auto *MDWP = !EnableMemorySSA
