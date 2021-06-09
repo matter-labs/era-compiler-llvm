ed8aa273e830747f5e2a7d371f655bd1eadb3b80
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-06-09 15:43:19 +0200

    Always pop to r0
    Introduce a pass that for each POP replaces VReg it defines to R0.
    Thus prevents failure of fast reg alloc and reduces register pressure.

---
+++ llvm/lib/Target/SyncVM/SyncVMDropUnusedRegisters.cpp
@@ -0,0 +1,76 @@
+//===-- SyncVMDropUnusedRegisters.cpp - Replace unused registers with r0 --===//
+//
+/// \file
+/// This file contains a pass that replaces unused definition with r0 so,
+/// register allocator doesn't need to care.
+//
+//===----------------------------------------------------------------------===//
+
+#include "SyncVM.h"
+
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+#include "llvm/Support/Debug.h"
+
+#include "SyncVMSubtarget.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "syncvm-drop-unused-registers"
+#define SYNCVM_DROP_UNUSED_REGS_NAME "SyncVM drop unused registers"
+
+namespace {
+
+class SyncVMDropUnusedRegisters : public MachineFunctionPass {
+public:
+  static char ID;
+  SyncVMDropUnusedRegisters() : MachineFunctionPass(ID) {}
+
+  const TargetRegisterInfo *TRI;
+
+  bool runOnMachineFunction(MachineFunction &Fn) override;
+
+  StringRef getPassName() const override {
+    return SYNCVM_DROP_UNUSED_REGS_NAME;
+  }
+
+private:
+  const TargetInstrInfo *TII;
+  LLVMContext *Context;
+};
+
+char SyncVMDropUnusedRegisters::ID = 0;
+
+} // namespace
+
+INITIALIZE_PASS(SyncVMDropUnusedRegisters, DEBUG_TYPE,
+                SYNCVM_DROP_UNUSED_REGS_NAME, false, false)
+
+bool SyncVMDropUnusedRegisters::runOnMachineFunction(MachineFunction &MF) {
+  LLVM_DEBUG(dbgs() << "********** SyncVM DROP UNUSED REGISTERS **********\n"
+                    << "********** Function: " << MF.getName() << '\n');
+
+  bool Changed = false;
+
+  TII = MF.getSubtarget<SyncVMSubtarget>().getInstrInfo();
+  assert(TII && "TargetInstrInfo must be a valid object");
+
+  Context = &MF.getFunction().getContext();
+
+  std::vector<MachineInstr *> Pseudos;
+  for (MachineBasicBlock &MBB : MF)
+    for (MachineInstr &MI : MBB) {
+      if (MI.getOpcode() == SyncVM::POP) {
+        MI.getOperand(0).setReg(SyncVM::R0);
+      }
+    }
+
+  LLVM_DEBUG(
+      dbgs() << "*******************************************************\n");
+  return Changed;
+}
+
+FunctionPass *llvm::createSyncVMDropUnusedRegistersPass() {
+  return new SyncVMDropUnusedRegisters();
+}
