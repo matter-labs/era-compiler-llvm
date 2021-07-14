831a1b40f807bfefd75f80321eabcca12cf5de0b
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-07-14 11:35:03 +0200

    Inform LLVM that syncvm.throw intrinsic throws
    Enable previoulsy disabled passes since they did wrong thing due to
    propagated nounwind attribute.

--- llvm/lib/Transforms/IPO/FunctionAttrs.cpp
+++ llvm/lib/Transforms/IPO/FunctionAttrs.cpp
@@ -1317,7 +1317,7 @@ static bool inferConvergent(const SCCNodeSet &SCCNodes) {
 static bool inferAttrsFromFunctionBodies(const SCCNodeSet &SCCNodes) {
   AttributeInferer AI;
 
-  if (!DisableNoUnwindInference)
+  if (false && !DisableNoUnwindInference)
     // Request to infer nounwind attribute for all the functions in the SCC if
     // every callsite within the SCC is not throwing (except for calls to
     // functions within the SCC). Note that nounwind attribute suffers from
