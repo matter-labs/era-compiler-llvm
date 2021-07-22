419a27fca6fbda982827e6a4ecf17df0ea9294b9
hedgar2017 <hedgar2017@gmail.com>
2021-07-22 23:04:25 +0400

    Add the build sequence for MacOS

--- build.sh
+++ build.sh
@@ -18,32 +18,58 @@ case "${1}" in
         ;;
 esac
 
-sudo apt --yes update
-sudo apt --yes install \
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew update
+    brew install cmake
+
     cmake \
-    clang-11 \
-    lld-11
+        -S 'llvm' \
+        -B "build-${DIRECTORY_SUFFIX}/" \
+        -G 'Unix Makefiles' \
+        -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DLLVM_TARGETS_TO_BUILD='X86' \
+        -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
+        -DLLVM_ENABLE_ASSERTIONS='On' \
+        -DLLVM_OPTIMIZED_TABLEGEN='On' \
+        -DLLVM_BUILD_DOCS='Off' \
+        -DLLVM_INCLUDE_DOCS='Off' \
+        -DLLVM_ENABLE_DOXYGEN='Off' \
+        -DLLVM_ENABLE_SPHINX='Off' \
+        -DLLVM_ENABLE_OCAMLDOC='Off' \
+        -DLLVM_ENABLE_BINDINGS='Off'
 
-cmake \
-    -S 'llvm' \
-    -B "build-${DIRECTORY_SUFFIX}/" \
-    -G 'Unix Makefiles' \
-    -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
-    -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
-    -DCMAKE_C_COMPILER='clang-11' \
-    -DCMAKE_CXX_COMPILER='clang++-11' \
-    -DLLVM_TARGETS_TO_BUILD='X86' \
-    -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
-    -DLLVM_ENABLE_ASSERTIONS='On' \
-    -DLLVM_OPTIMIZED_TABLEGEN='On' \
-    -DLLVM_USE_LINKER='lld-11' \
-    -DLLVM_BUILD_DOCS='Off' \
-    -DLLVM_INCLUDE_DOCS='Off' \
-    -DLLVM_ENABLE_DOXYGEN='Off' \
-    -DLLVM_ENABLE_SPHINX='Off' \
-    -DLLVM_ENABLE_OCAMLDOC='Off' \
-    -DLLVM_ENABLE_BINDINGS='Off'
+    cd "build-${DIRECTORY_SUFFIX}/"
+    make -j "$(sysctl -n hw.logicalcpu)"
+else
+    sudo apt --yes update
+    sudo apt --yes install \
+        cmake \
+        clang-11 \
+        lld-11
 
-cd "build-${DIRECTORY_SUFFIX}/"
-make -j "$(nproc)"
+    cmake \
+        -S 'llvm' \
+        -B "build-${DIRECTORY_SUFFIX}/" \
+        -G 'Unix Makefiles' \
+        -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DCMAKE_C_COMPILER='clang-11' \
+        -DCMAKE_CXX_COMPILER='clang++-11' \
+        -DLLVM_TARGETS_TO_BUILD='X86' \
+        -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
+        -DLLVM_ENABLE_ASSERTIONS='On' \
+        -DLLVM_OPTIMIZED_TABLEGEN='On' \
+        -DLLVM_USE_LINKER='lld-11' \
+        -DLLVM_BUILD_DOCS='Off' \
+        -DLLVM_INCLUDE_DOCS='Off' \
+        -DLLVM_ENABLE_DOXYGEN='Off' \
+        -DLLVM_ENABLE_SPHINX='Off' \
+        -DLLVM_ENABLE_OCAMLDOC='Off' \
+        -DLLVM_ENABLE_BINDINGS='Off'
+
+    cd "build-${DIRECTORY_SUFFIX}/"
+    make -j "$(nproc)"
+fi
+
 sudo make install
