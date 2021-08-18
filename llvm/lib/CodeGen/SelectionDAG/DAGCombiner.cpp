44ccfbca5a4b8b8f336d9caa571ad8cace17175d
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2021-08-18 20:53:14 +0200

    Remove local changes from DAG combiner

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -2222,8 +2222,6 @@ SDValue DAGCombiner::visitADDLike(SDNode *N) {
     // We don't transform this pattern:
     //   add (zext i1 X), -1 -> sext (not i1 X)
     // because most (?) targets generate better code for the zext form.
-// SyncVM local change begin
-#if 0
     if (N0.getOpcode() == ISD::SIGN_EXTEND && N0.hasOneUse() &&
         isOneOrOneSplat(N1)) {
       SDValue X = N0.getOperand(0);
@@ -2235,7 +2233,6 @@ SDValue DAGCombiner::visitADDLike(SDNode *N) {
         return DAG.getNode(ISD::ZERO_EXTEND, DL, VT, Not);
       }
     }
-#endif
 
     // Fold (add (or x, c0), c1) -> (add x, (c0 + c1)) if (or x, c0) is
     // equivalent to (add x, c0).
@@ -2331,8 +2328,6 @@ SDValue DAGCombiner::visitADDLike(SDNode *N) {
   if (SimplifyDemandedBits(SDValue(N, 0)))
     return SDValue(N, 0);
 
-// SyncVM local begin
-#if 0
   if (isOneOrOneSplat(N1)) {
     // fold (add (xor a, -1), 1) -> (sub 0, a)
     if (isBitwiseNot(N0))
@@ -2375,8 +2370,6 @@ SDValue DAGCombiner::visitADDLike(SDNode *N) {
     SDValue Xor = DAG.getNode(ISD::XOR, DL, VT, N0.getOperand(1), N1);
     return DAG.getNode(ISD::ADD, DL, VT, Xor, N0.getOperand(0));
   }
-#endif
-// SyncVM local end
 
   if (SDValue Combined = visitADDLikeCommutative(N0, N1, N))
     return Combined;
@@ -2402,14 +2395,10 @@ SDValue DAGCombiner::visitADD(SDNode *N) {
   if (SDValue V = foldAddSubOfSignBit(N, DAG))
     return V;
 
-// SyncVM local begin
-#if 0
   // fold (a+b) -> (a|b) iff a and b share no bits.
   if ((!LegalOperations || TLI.isOperationLegal(ISD::OR, VT)) &&
       DAG.haveNoCommonBitsSet(N0, N1))
     return DAG.getNode(ISD::OR, DL, VT, N0, N1);
-#endif
-// SyncVM local end
 
   // Fold (add (vscale * C0), (vscale * C1)) to (vscale * (C0 + C1)).
   if (N0.getOpcode() == ISD::VSCALE && N1.getOpcode() == ISD::VSCALE) {
@@ -2553,16 +2542,12 @@ SDValue DAGCombiner::visitADDLikeCommutative(SDValue N0, SDValue N1,
   //   add (add x, 1), y
   // And if the target does not like this form then turn into:
   //   sub y, (xor x, -1)
-  // SyncVM local begin
-#if 0
   if (!TLI.preferIncOfAddToSubOfNot(VT) && N0.hasOneUse() &&
       N0.getOpcode() == ISD::ADD && isOneOrOneSplat(N0.getOperand(1))) {
     SDValue Not = DAG.getNode(ISD::XOR, DL, VT, N0.getOperand(0),
                               DAG.getAllOnesConstant(DL, VT));
     return DAG.getNode(ISD::SUB, DL, VT, N1, Not);
   }
-#endif
-  // SyncVM local end
 
   // Hoist one-use subtraction by non-opaque constant:
   //   (x - C) + y  ->  (x + y) - C
@@ -3129,12 +3114,8 @@ SDValue DAGCombiner::visitSUB(SDNode *N) {
   }
 
   // Canonicalize (sub -1, x) -> ~x, i.e. (xor x, -1)
-  // SyncVM local begin
-#if 0
   if (isAllOnesOrAllOnesSplat(N0))
     return DAG.getNode(ISD::XOR, DL, VT, N1, N0);
-#endif
-  // SyncVM local end
 
   // fold (A - (0-B)) -> A+B
   if (N1.getOpcode() == ISD::SUB && isNullOrNullSplat(N1.getOperand(0)))
@@ -3267,15 +3248,11 @@ SDValue DAGCombiner::visitSUB(SDNode *N) {
     return V;
 
   // (x - y) - 1  ->  add (xor y, -1), x
-  // SyncVM local begin
-#if 0
   if (N0.hasOneUse() && N0.getOpcode() == ISD::SUB && isOneOrOneSplat(N1)) {
     SDValue Xor = DAG.getNode(ISD::XOR, DL, VT, N0.getOperand(1),
                               DAG.getAllOnesConstant(DL, VT));
     return DAG.getNode(ISD::ADD, DL, VT, Xor, N0.getOperand(0));
   }
-#endif
-  // SyncVM local end
 
   // Look for:
   //   sub y, (xor x, -1)
@@ -3451,13 +3428,9 @@ SDValue DAGCombiner::visitSUBC(SDNode *N) {
     return CombineTo(N, N0, DAG.getNode(ISD::CARRY_FALSE, DL, MVT::Glue));
 
   // Canonicalize (sub -1, x) -> ~x, i.e. (xor x, -1) + no borrow
-  // SyncVM local begin
-#if 0
   if (isAllOnesConstant(N0))
     return CombineTo(N, DAG.getNode(ISD::XOR, DL, VT, N1, N0),
                      DAG.getNode(ISD::CARRY_FALSE, DL, MVT::Glue));
-#endif
-  // SyncVM local end
 
   return SDValue();
 }
@@ -3611,8 +3584,6 @@ SDValue DAGCombiner::visitMUL(SDNode *N) {
 
 // TODO: The following code needs to be enabled on the architectures where it's
 // profitable to use shl. Consider implementing a check for it.
-// SyncVM local begin
-#if 0
   // fold (mul x, (1 << c)) -> x << c
   if (isConstantOrConstantVector(N1, /*NoOpaques*/ true) &&
       DAG.isKnownToBeAPowerOfTwo(N1) &&
@@ -3669,8 +3640,6 @@ SDValue DAGCombiner::visitMUL(SDNode *N) {
       return R;
     }
   }
-#endif
-// SyncVM local end
 
   // (mul (shl X, c1), c2) -> (mul X, c2 << c1)
   if (N0.getOpcode() == ISD::SHL &&
@@ -3947,9 +3916,6 @@ SDValue DAGCombiner::visitSDIVLike(SDValue N0, SDValue N1, SDNode *N) {
     return false;
   };
 
-// TODO: Implement architecture / profitability check
-// SyncVM local begin
-#if 0
   // fold (sdiv X, pow2) -> simple ops after legalize
   // FIXME: We check for the exact bit here because the generic lowering gives
   // better results in that case. The target-specific lowering should learn how
@@ -4000,8 +3966,6 @@ SDValue DAGCombiner::visitSDIVLike(SDValue N0, SDValue N1, SDNode *N) {
     SDValue Res = DAG.getSelect(DL, VT, IsNeg, Sub, Sra);
     return Res;
   }
-#endif
-// SyncVM local end
 
   // If integer divide is expensive and we satisfy the requirements, emit an
   // alternate sequence.  Targets may check function attributes for size/speed
@@ -4074,9 +4038,6 @@ SDValue DAGCombiner::visitUDIVLike(SDValue N0, SDValue N1, SDNode *N) {
   SDLoc DL(N);
   EVT VT = N->getValueType(0);
 
-// TODO: Implement architecture / profitability check
-// SyncVM local begin
-#if 0
   // fold (udiv x, (1 << c)) -> x >>u c
   if (isConstantOrConstantVector(N1, /*NoOpaques*/ true) &&
       DAG.isKnownToBeAPowerOfTwo(N1)) {
@@ -4088,8 +4049,6 @@ SDValue DAGCombiner::visitUDIVLike(SDValue N0, SDValue N1, SDNode *N) {
     AddToWorklist(Trunc.getNode());
     return DAG.getNode(ISD::SRL, DL, VT, N0, Trunc);
   }
-#endif
-// SyncVM local end
 
   // fold (udiv x, (shl c, y)) -> x >>u (log2(c)+y) iff c is power of 2
   if (N1.getOpcode() == ISD::SHL) {
@@ -4109,6 +4068,7 @@ SDValue DAGCombiner::visitUDIVLike(SDValue N0, SDValue N1, SDNode *N) {
   }
 
 // TODO: Implement architecture / profitability check
+// TODO: produces mulhu, which is not currently supported by SyncVM
 // SyncVM local begin
 #if 0
   // fold (udiv x, c) -> alternate
@@ -4156,8 +4116,6 @@ SDValue DAGCombiner::visitREM(SDNode *N) {
     if (DAG.SignBitIsZero(N1) && DAG.SignBitIsZero(N0))
       return DAG.getNode(ISD::UREM, DL, VT, N0, N1);
   } else {
-// SyncVM local begin
-#if 0
     SDValue NegOne = DAG.getAllOnesConstant(DL, VT);
     if (DAG.isKnownToBeAPowerOfTwo(N1)) {
       // fold (urem x, pow2) -> (and x, pow2-1)
@@ -4172,8 +4130,6 @@ SDValue DAGCombiner::visitREM(SDNode *N) {
       AddToWorklist(Add.getNode());
       return DAG.getNode(ISD::AND, DL, VT, N0, Add);
     }
-#endif
-// SyncVM local end
   }
 
   AttributeList Attr = DAG.getMachineFunction().getFunction().getAttributes();
@@ -8953,8 +8909,6 @@ SDValue DAGCombiner::foldSelectOfConstants(SDNode *N) {
     return SDValue();
   }
 
-// SyncVM local begin
-#if 0
   // fold (select Cond, 0, 1) -> (xor Cond, 1)
   // We can't do this reliably if integer based booleans have different contents
   // to floating point based booleans. This is because we can't tell whether we
@@ -8976,8 +8930,6 @@ SDValue DAGCombiner::foldSelectOfConstants(SDNode *N) {
       return NotCond;
     return DAG.getZExtOrTrunc(NotCond, DL, VT);
   }
-#endif
-// SyncVM local end
 
   return SDValue();
 }
@@ -8994,8 +8946,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
   if (SDValue V = DAG.simplifySelect(N0, N1, N2))
     return V;
 
-// SyncVM local begin
-#if 0
   // fold (select X, X, Y) -> (or X, Y)
   // fold (select X, 1, Y) -> (or C, Y)
   if (VT == VT0 && VT == MVT::i1 && (N0 == N1 || isOneConstant(N1)))
@@ -9020,8 +8970,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
   // fold (select X, Y, 0) -> (and X, Y)
   if (VT == VT0 && VT == MVT::i1 && (N0 == N2 || isNullConstant(N2)))
     return DAG.getNode(ISD::AND, DL, VT, N0, N1);
-#endif
-  // SyncVM local end
 
   // If we can fold this based on the true/false value, do so.
   if (SimplifySelectOps(N, N1, N2))
@@ -9067,8 +9015,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
     }
 
     // select Cond0, (select Cond1, X, Y), Y -> select (and Cond0, Cond1), X, Y
-    // SyncVM local begin
-#if 0
     if (N1->getOpcode() == ISD::SELECT && N1->hasOneUse()) {
       SDValue N1_0 = N1->getOperand(0);
       SDValue N1_1 = N1->getOperand(1);
@@ -9087,8 +9033,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
         }
       }
     }
-#endif
-    //SyncVM local end
     // select Cond0, X, (select Cond1, X, Y) -> select (or Cond0, Cond1), X, Y
     if (N2->getOpcode() == ISD::SELECT && N2->hasOneUse()) {
       SDValue N2_0 = N2->getOperand(0);
@@ -9130,8 +9074,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
                                                 CC, TLI, DAG))
         return FMinMax;
 
-    // SyncVM local begin
-#if 0
     // Use 'unsigned add with overflow' to optimize an unsigned saturating add.
     // This is conservatively limited to pre-legal-operations to give targets
     // a chance to reverse the transform if they want to do that. Also, it is
@@ -9160,8 +9102,6 @@ SDValue DAGCombiner::visitSELECT(SDNode *N) {
         return DAG.getSelect(DL, VT, UAO.getValue(1), N1, UAO.getValue(0));
       }
     }
-#endif
-    // SyncVM local end
 
     if (TLI.isOperationLegal(ISD::SELECT_CC, VT) ||
         (!LegalOperations &&
