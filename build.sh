61082799ad02cb9c63920bab32072b88f75b75d9
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-23 11:54:12 +0300

    Add the Arch Linux section to the building script

--- build.sh
+++ build.sh
@@ -41,9 +41,35 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     cd "build-${DIRECTORY_SUFFIX}/"
     make -j "$(sysctl -n hw.logicalcpu)"
-fi
+elif [[ -f '/etc/arch-release' ]]; then
+    sudo pacman --sync --refresh --sysupgrade --noconfirm \
+        cmake \
+	clang \
+	lld
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    cmake \
+        -S 'llvm' \
+        -B "build-${DIRECTORY_SUFFIX}/" \
+        -G 'Unix Makefiles' \
+        -DCMAKE_INSTALL_PREFIX="${HOME}/opt/llvm-${DIRECTORY_SUFFIX}/" \
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DCMAKE_C_COMPILER='clang' \
+        -DCMAKE_CXX_COMPILER='clang++' \
+        -DLLVM_TARGETS_TO_BUILD='X86' \
+        -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD='SyncVM' \
+        -DLLVM_ENABLE_ASSERTIONS='On' \
+        -DLLVM_OPTIMIZED_TABLEGEN='On' \
+        -DLLVM_USE_LINKER='lld' \
+        -DLLVM_BUILD_DOCS='Off' \
+        -DLLVM_INCLUDE_DOCS='Off' \
+        -DLLVM_ENABLE_DOXYGEN='Off' \
+        -DLLVM_ENABLE_SPHINX='Off' \
+        -DLLVM_ENABLE_OCAMLDOC='Off' \
+        -DLLVM_ENABLE_BINDINGS='Off'
+
+    cd "build-${DIRECTORY_SUFFIX}/"
+    make -j "$(nproc)"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt --yes update
     sudo apt --yes install \
         cmake \
