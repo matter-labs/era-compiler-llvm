078fa47ffd6b142924cc3dca0504e1388c9282cc
Alan Li <alan.li@me.com>
2022-01-05 21:13:22 -0500

    Lower `SMUL_LOHI` and `UMUL_LOHI`

--- llvm/lib/Target/SyncVM/SyncVMISelDAGToDAG.cpp
+++ llvm/lib/Target/SyncVM/SyncVMISelDAGToDAG.cpp
@@ -372,6 +372,37 @@ void SyncVMDAGToDAGISel::Select(SDNode *Node) {
     return;
   }
 
+  auto has_uses = [](SDNode *Node) {
+    bool val0_used = Node->hasAnyUseOfValue(0);
+    bool val1_used = Node->hasAnyUseOfValue(1);
+
+    return std::make_pair(val0_used, val1_used);
+  };
+
+  auto muldiv_expander = [&](SDNode *Node, uint32_t opcode1, uint32_t opcode2) {
+    auto uses = has_uses(Node);
+    bool val0_used = uses.first;
+    bool val1_used = uses.second;
+
+    if (val0_used && !val1_used) {
+      auto op1 = CurDAG->getMachineNode(
+          opcode1, DL, MVT::i256, Node->getOperand(0), Node->getOperand(1));
+
+      ReplaceUses(SDValue(Node, 0), SDValue(op1, 0));
+      CurDAG->RemoveDeadNode(Node);
+      return true;
+    }
+
+    if (!val0_used && val1_used) {
+      auto op2 = CurDAG->getMachineNode(
+          opcode2, DL, MVT::i256, Node->getOperand(0), Node->getOperand(1));
+      ReplaceUses(SDValue(Node, 1), SDValue(op2, 0));
+      CurDAG->RemoveDeadNode(Node);
+      return true;
+    }
+    return false;
+  };
+
   // Few custom selection stuff.
   switch (Node->getOpcode()) {
   default:
@@ -391,26 +422,15 @@ void SyncVMDAGToDAGISel::Select(SDNode *Node) {
     return;
   }
   case ISD::UDIVREM: {
-    // convert UDIVREM to UDIV or UREM if only one of the results are used
-    bool val0_used = Node->hasAnyUseOfValue(0);
-    bool val1_used = Node->hasAnyUseOfValue(1);
-
-    if (val0_used && !val1_used) {
-      auto udiv =
-          CurDAG->getMachineNode(SyncVM::DIVrrrz, DL, MVT::i256,
-                                 Node->getOperand(0), Node->getOperand(1));
-
-      ReplaceUses(SDValue(Node, 0), SDValue(udiv, 0));
-      CurDAG->RemoveDeadNode(Node);
+    // redirect unused variable to $r0
+    if (muldiv_expander(Node, SyncVM::DIVrrrz, SyncVM::REMrrzr)) {
       return;
     }
-
-    if (!val0_used && val1_used) {
-      auto urem =
-          CurDAG->getMachineNode(SyncVM::REMrrzr, DL, MVT::i256,
-                                 Node->getOperand(0), Node->getOperand(1));
-      ReplaceUses(SDValue(Node, 1), SDValue(urem, 0));
-      CurDAG->RemoveDeadNode(Node);
+    break;
+  }
+  case ISD::UMUL_LOHI: {
+    // redirect unused variable to $r0
+    if (muldiv_expander(Node, SyncVM::MULrrrz, SyncVM::MULrrzr)) {
       return;
     }
     break;
--- llvm/lib/Target/SyncVM/SyncVMISelLowering.cpp
+++ llvm/lib/Target/SyncVM/SyncVMISelLowering.cpp
@@ -53,10 +53,15 @@ SyncVMTargetLowering::SyncVMTargetLowering(const TargetMachine &TM,
   setOperationAction(ISD::SELECT_CC, MVT::i256, Custom);
   setOperationAction(ISD::TRUNCATE, MVT::i64, Promote);
 
+  // special handling of udiv/urem
   setOperationAction(ISD::UDIV, MVT::i256, Expand);
   setOperationAction(ISD::UREM, MVT::i256, Expand);
-  setOperationAction(ISD::UMUL_LOHI, MVT::i256, Legal);
   setOperationAction(ISD::UDIVREM, MVT::i256, Legal);
+
+  // special handling of umulxx
+  setOperationAction(ISD::MUL, MVT::i256, Expand);
+  setOperationAction(ISD::SMUL_LOHI, MVT::i256, Expand);
+  setOperationAction(ISD::UMUL_LOHI, MVT::i256, Legal);
 
   // SyncVM lacks of native support for signed operations.
   setOperationAction(ISD::SRA, MVT::i256, Custom);
@@ -253,13 +258,15 @@ SyncVMTargetLowering::LowerCall(TargetLowering::CallLoweringInfo &CLI,
   CallingConv::ID CallConv = CLI.CallConv;
   bool IsVarArg = CLI.IsVarArg;
   SmallVector<SDValue, 12> MemOpChains;
-  const Function* CalleeF = [&Callee, &DAG](){
+  const Function *CalleeF = [&Callee, &DAG]() {
     if (auto *GA = dyn_cast<GlobalAddressSDNode>(Callee))
       return cast<Function>(GA->getGlobal());
     else if (auto *ES = dyn_cast<ExternalSymbolSDNode>(Callee))
-      return cast<Function>(cast<GlobalAddressSDNode>(DAG.getSymbolFunctionGlobalAddress(Callee))->getGlobal());
+      return cast<Function>(
+          cast<GlobalAddressSDNode>(DAG.getSymbolFunctionGlobalAddress(Callee))
+              ->getGlobal());
     llvm_unreachable("Unexpected node for a call");
-    return (const Function*) nullptr;
+    return (const Function *)nullptr;
   }();
 
   // TODO: SyncVM target does not yet support tail call optimization.
