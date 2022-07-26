09861c1e7cd3064540805e1cec1024de12bebe58
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2022-07-26 13:49:57 +0200

    Disable jump table building for SyncVM
    Selection DAG Builder used to build jump tables for minsize attributed
    function when switch cases are dense. SyncVM doesn't allow jump tables
    yet.

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -11278,6 +11278,12 @@ void SelectionDAGBuilder::visitSwitch(const SwitchInst &SI) {
     return;
   }
 
+  // SyncVM local begin
+  // TODO: CPR-688 SyncVM can build jump tables, though the constants are 4
+  // times as expensive as instructions in terms of code size. For hot pieces of
+  // code it still makes sense.
+  if (!TM.getTargetTriple().isSyncVM())
+  // SyncVM local end
   SL->findJumpTables(Clusters, &SI, DefaultMBB, DAG.getPSI(), DAG.getBFI());
   SL->findBitTestClusters(Clusters, &SI);
 
