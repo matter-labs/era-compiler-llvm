91b7157e133903d292db4ba61d8563ff92adee91
Alan Li <alan.li@me.com>
2022-08-11 13:06:37 -0700

    Implement ptr arithmetic operations in backend
    * adding ptr.add and ptr.pack
    * adding corresponding intrinsics
    * GEP on AS 3 pointers will be converted to intrinsics before lowering
    
    Co-authored with Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>

--- llvm/lib/Target/SyncVM/SyncVMCodegenPrepare.cpp
+++ llvm/lib/Target/SyncVM/SyncVMCodegenPrepare.cpp
@@ -4,13 +4,18 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
-#include "llvm/IR/IntrinsicsHexagon.h"
 #include "llvm/Pass.h"
 #include "llvm/Transforms/Scalar.h"
+
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/GetElementPtrTypeIterator.h"
+#include "llvm/IR/IntrinsicsSyncVM.h"
 
 #include "SyncVM.h"
 
 using namespace llvm;
+
+#define DEBUG_TYPE "syncvm-codegen-prepare"
 
 namespace llvm {
 FunctionPass *createSyncVMCodegenPrepare();
@@ -26,8 +31,10 @@ public:
   }
   bool runOnFunction(Function &F) override;
 
+  bool convertPointerArithmetics(Function &F);
+
   StringRef getPassName() const override {
-    return "Revert usopported instcombine changes";
+    return "Final transformations before code generation";
   }
 
   void getAnalysisUsage(AnalysisUsage &AU) const override {
@@ -39,10 +46,11 @@ public:
 char SyncVMCodegenPrepare::ID = 0;
 
 INITIALIZE_PASS(SyncVMCodegenPrepare, "syncvm-codegen-prepare",
-                "Revert usopported instcombine changes", false, false)
+                "Final transformations before code generation", false, false)
 
 bool SyncVMCodegenPrepare::runOnFunction(Function &F) {
   bool Changed = false;
+
   std::vector<Instruction *> Replaced;
   for (auto &BB : F)
     for (auto II = BB.begin(); II != BB.end(); ++II) {
@@ -128,7 +136,69 @@ bool SyncVMCodegenPrepare::runOnFunction(Function &F) {
     }
   for (auto *I : Replaced)
     I->eraseFromParent();
+
+  // convert pointer arithmetics to intrinsics
+  Changed |= convertPointerArithmetics(F);
   return Changed;
+}
+
+bool SyncVMCodegenPrepare::convertPointerArithmetics(Function &F) {
+  const DataLayout *DL = &F.getParent()->getDataLayout();
+  auto computeOffset = [&](GetElementPtrInst *GEP, IRBuilder<> &Builder) {
+    Value *TmpOffset = Builder.getIntN(256, 0);
+    APInt Offset(256, 0);
+    if (GEP->accumulateConstantOffset(*DL, Offset)) {
+      // the 32 here is the word length for zkEVM
+      TmpOffset = Builder.getIntN(256, Offset.getZExtValue());
+    } else {
+      for (gep_type_iterator GTI = gep_type_begin(GEP), E = gep_type_end(GEP);
+           GTI != E; ++GTI) {
+        Value *Op = GTI.getOperand();
+        uint64_t S = DL->getTypeAllocSize(GTI.getIndexedType());
+
+        // rely on folding down the pipeline to fold it.
+        Value *Rhs = Builder.CreateMul(Builder.getIntN(256, S), Op);
+        TmpOffset = Builder.CreateAdd(TmpOffset, Rhs);
+      }
+    }
+    assert(TmpOffset != nullptr);
+    return TmpOffset;
+  };
+
+  std::vector<Instruction *> Replaced;
+
+  // look for GEP and convert them to PTR_ADD
+  for (auto &BB : F) {
+    for (auto &I : BB) {
+      if (auto *GEP = dyn_cast<GetElementPtrInst>(&I)) {
+        // Address Space must be return data address space
+        if (GEP->getPointerAddressSpace() != SyncVMAS::AS_GENERIC)
+          continue;
+
+        Value *Base = GEP->getOperand(0);
+
+        IRBuilder<> Builder(&I);
+        Value *Offset = computeOffset(GEP, Builder);
+        Base = Builder.CreateBitCast(Base, Builder.getInt8PtrTy(3));
+
+        auto *PtrAdd =
+            Intrinsic::getDeclaration(F.getParent(), Intrinsic::syncvm_ptr_add);
+
+        Value *NewI = Builder.CreateCall(PtrAdd, {Base, Offset});
+        NewI = Builder.CreateBitCast(NewI, GEP->getType());
+
+        LLVM_DEBUG(dbgs() << "    Converted GEP:"; GEP->dump();
+                   dbgs() << "    to: "; NewI->dump());
+        GEP->replaceAllUsesWith(NewI);
+        Replaced.push_back(GEP);
+      }
+    }
+  }
+
+  for (Instruction *I : Replaced)
+    I->eraseFromParent();
+
+  return !Replaced.empty();
 }
 
 FunctionPass *llvm::createSyncVMCodegenPreparePass() {
