ca808da25fbbf4b4bd2e964ec84f471850b1e9ff
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2022-08-21 23:12:51 +0200

    Update memcpy expansion
    Expand memcpy using gep instead of pointer arithrmetic to access
    elements, and move the logic to SyncVM target instead of making local
    changes in LowerMemIntrinsics.
    The patch also corrects the data layout for SyncVM.

--- llvm/lib/Target/SyncVM/SyncVMTargetMachine.cpp
+++ llvm/lib/Target/SyncVM/SyncVMTargetMachine.cpp
@@ -43,7 +43,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSyncVMTarget() {
 }
 
 static std::string computeDataLayout() {
-  return "E-p:256:256-i8:256:256:256-i256:256:256-S32-a:256:256";
+  return "E-p:256:256-i256:256:256-S32-a:256:256";
 }
 
 static Reloc::Model getEffectiveRelocModel(Optional<Reloc::Model> RM) {
