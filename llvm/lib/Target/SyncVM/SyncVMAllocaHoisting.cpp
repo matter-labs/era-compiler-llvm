c1c6dde7574f2c1f0a7367bfbffc97386de1c8c3
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2022-11-03 12:08:17 +0200

    [NFC] Extract alloca hoisting as an utility

--- llvm/lib/Target/SyncVM/SyncVMAllocaHoisting.cpp
+++ llvm/lib/Target/SyncVM/SyncVMAllocaHoisting.cpp
@@ -11,9 +11,8 @@
 #include "SyncVM.h"
 
 #include "llvm/CodeGen/StackProtector.h"
-#include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/Instructions.h"
+#include "llvm/Transforms/Utils/AllocaHoisting.h"
 
 using namespace llvm;
 
@@ -37,22 +36,8 @@ public:
 };
 } // namespace
 
-bool SyncVMAllocaHoisting::runOnFunction(Function &function) {
-  bool functionModified = false;
-  Function::iterator I = function.begin();
-  Instruction *firstTerminatorInst = (I++)->getTerminator();
-
-  for (Function::iterator E = function.end(); I != E; ++I) {
-    for (BasicBlock::iterator BI = I->begin(), BE = I->end(); BI != BE;) {
-      AllocaInst *allocaInst = dyn_cast<AllocaInst>(BI++);
-      if (allocaInst && isa<ConstantInt>(allocaInst->getArraySize())) {
-        allocaInst->moveBefore(firstTerminatorInst);
-        functionModified = true;
-      }
-    }
-  }
-
-  return functionModified;
+bool SyncVMAllocaHoisting::runOnFunction(Function &F) {
+  return HoistAllocaToEntry(F);
 }
 
 char SyncVMAllocaHoisting::ID = 0;
