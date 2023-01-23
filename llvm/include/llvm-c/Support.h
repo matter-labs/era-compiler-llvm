53b5188ad9da934efcce378d53b697f85c7449ea
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2023-01-23 17:48:05 +0200

    Track commit id as part of the version
    * Export commit ID getting API as LLVMPrintCommitIDTo.
    * Print commit id in LLVM tool --version message.

--- llvm/include/llvm-c/Support.h
+++ llvm/include/llvm-c/Support.h
@@ -63,6 +63,14 @@ void *LLVMSearchForAddressOfSymbol(const char *symbolName);
  */
 void LLVMAddSymbol(const char *symbolName, void *symbolValue);
 
+// SyncVM local begin
+/**
+ * Print git commit id to Buf.
+ * Return sprintf exit code.
+ */
+int LLVMPrintCommitIDTo(char* Buf);
+// SyncVM local end
+
 /**
  * @}
  */
