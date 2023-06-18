cdf5a24c9064d5f5da84b1bfe3d812fe8b502df5
Pavel Kopyl <pavelkopyl@gmail.com>
2023-06-19 00:20:00 +0200

    [EVM] Addign frameindex lowering.
    Please, note this implementation assumes the stack is located in
    AS0 memeory and is intended for getting MIR with virtual register.
    We need to design how the frame slots to be represented in the stackified code.
    We can either use EVM stack (though EVM doesn't seem to have instructions to
    do this in a general case), or we can allocate a stack area in the AS0 memory.

--- llvm/lib/Target/EVM/EVMRegisterInfo.cpp
+++ llvm/lib/Target/EVM/EVMRegisterInfo.cpp
@@ -7,7 +7,9 @@
 
 #include "EVMFrameLowering.h"
 #include "EVMInstrInfo.h"
+#include "EVMSubtarget.h"
 #include "MCTargetDesc/EVMMCTargetDesc.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
 using namespace llvm;
 
 #define DEBUG_TYPE "evm-reg-info"
@@ -33,11 +35,41 @@ EVMRegisterInfo::getReservedRegs(const MachineFunction & /*MF*/) const {
 void EVMRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
                                           int SPAdj, unsigned FIOperandNum,
                                           RegScavenger *RS) const {
-  llvm_unreachable("Subrotines are not supported yet");
+  assert(SPAdj == 0);
+  MachineInstr &MI = *II;
+
+  MachineBasicBlock &MBB = *MI.getParent();
+  MachineFunction &MF = *MBB.getParent();
+  MachineRegisterInfo &MRI = MF.getRegInfo();
+  int FrameIndex = MI.getOperand(FIOperandNum).getIndex();
+  const MachineFrameInfo &MFI = MF.getFrameInfo();
+  int64_t FrameOffset = MFI.getObjectOffset(FrameIndex);
+  const auto *TII = MF.getSubtarget<EVMSubtarget>().getInstrInfo();
+
+  assert(FrameOffset >= 0 && "FrameOffset < 0");
+  assert(FrameOffset < static_cast<int64_t>(MFI.getStackSize()) &&
+         "FrameOffset overflows stack size");
+  assert(MFI.getObjectSize(FrameIndex) != 0 &&
+         "We assume that variable-sized objects have already been lowered, "
+         "and don't use FrameIndex operands.");
+
+  Register FrameRegister = getFrameRegister(MF);
+  Register FIRegOperand = FrameRegister;
+  if (FrameOffset > 0) {
+    FIRegOperand = MRI.createVirtualRegister(&EVM::GPRRegClass);
+    Register OffsetReg = MRI.createVirtualRegister(&EVM::GPRRegClass);
+    BuildMI(MBB, MI, II->getDebugLoc(), TII->get(EVM::CONST_I256), OffsetReg)
+        .addImm(FrameOffset);
+    BuildMI(MBB, MI, II->getDebugLoc(), TII->get(EVM::ADD), FIRegOperand)
+        .addReg(FrameRegister)
+        .addReg(OffsetReg);
+  }
+
+  MI.getOperand(FIOperandNum).ChangeToRegister(FIRegOperand, /*isDef=*/false);
 }
 
 Register EVMRegisterInfo::getFrameRegister(const MachineFunction &MF) const {
-  return 0;
+  return EVM::SP;
 }
 
 const TargetRegisterClass *
