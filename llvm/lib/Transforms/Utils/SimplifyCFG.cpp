ba6727b1b626724057c173426baf4293ee968e73
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2023-12-05 15:40:36 +0200

    Remove a local change from simplifyCondBranch
    To be squashed with c42b8a5, 28015d6, 125fbb7.

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -6836,8 +6836,6 @@ bool SimplifyCFGOpt::simplifyCondBranch(BranchInst *BI, IRBuilder<> &Builder) {
     return false;
 
   // Conditional branch
-  // EraVM local begin
-   #if 0
   if (isValueEqualityComparison(BI)) {
     // If we only have one predecessor, and if it is a branch on this value,
     // see if that predecessor totally determines the outcome of this
@@ -6862,8 +6860,6 @@ bool SimplifyCFGOpt::simplifyCondBranch(BranchInst *BI, IRBuilder<> &Builder) {
   // Try to turn "br (X == 0 | X == 1), T, F" into a switch instruction.
   if (SimplifyBranchOnICmpChain(BI, Builder, DL))
     return true;
-#endif
-// EraVM local end
 
   // If this basic block has dominating predecessor blocks and the dominating
   // blocks' conditions imply BI's condition, we know the direction of BI.
