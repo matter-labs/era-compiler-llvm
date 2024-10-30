123fe4b90483089a28cce00d9886de6d3603df65
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2024-10-30 09:38:34 +0200

    [UpdateTestChecks] Add EVM support in update_llc_test_checks.py

--- llvm/utils/UpdateTestChecks/asm.py
+++ llvm/utils/UpdateTestChecks/asm.py
@@ -263,6 +263,12 @@ ASM_FUNCTION_ERAVM_RE = re.compile(
     r'\s*; -- End function',
     flags=(re.M | re.S))
 
+ASM_FUNCTION_EVM_RE = re.compile(
+    r'^_?(?P<func>[^:]+):[ \t]*;+[ \t]*@"?(?P=func)"?\n[^:]*?'
+    r'(?P<body>.*?)\n'
+    r'\s*; -- End function',
+    flags=(re.M | re.S))
+
 SCRUB_X86_SHUFFLES_RE = re.compile(
     r"^(\s*\w+) [^#\n]+#+ ((?:[xyz]mm\d+|mem)( \{%k\d+\}( \{z\})?)? = .*)$", flags=re.M
 )
@@ -541,6 +547,16 @@ def scrub_asm_eravm(asm, args):
   asm = common.SCRUB_TRAILING_WHITESPACE_RE.sub(r'', asm)
   return asm
 
+def scrub_asm_evm(asm, args):
+  # Scrub runs of whitespace out of the assembly, but leave the leading
+  # whitespace in place.
+  asm = common.SCRUB_WHITESPACE_RE.sub(r' ', asm)
+  # Expand the tabs used for indentation.
+  asm = string.expandtabs(asm, 2)
+  # Strip trailing whitespace.
+  asm = common.SCRUB_TRAILING_WHITESPACE_RE.sub(r'', asm)
+  return asm
+
 # Returns a tuple of a scrub function and a function regex. Scrub function is
 # used to alter function body in some way, for example, remove trailing spaces.
 # Function regex is used to match function name, body, etc. in raw llc output.
@@ -595,6 +611,7 @@ def get_run_handler(triple):
         "loongarch32": (scrub_asm_loongarch, ASM_FUNCTION_LOONGARCH_RE),
         "loongarch64": (scrub_asm_loongarch, ASM_FUNCTION_LOONGARCH_RE),
         "eravm" : (scrub_asm_eravm, ASM_FUNCTION_ERAVM_RE),
+        "evm" : (scrub_asm_evm, ASM_FUNCTION_EVM_RE),
     }
     handler = None
     best_prefix = ""
