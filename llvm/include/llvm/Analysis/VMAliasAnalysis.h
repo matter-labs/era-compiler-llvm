daff34ae1920267142bf69c16b76f7e4c5d59306
Vladimir Radosavljevic <vr@matterlabs.dev>
2024-10-31 14:01:50 +0100

    Generalize EraVMAAResult and move it to the common part
    This way, we can share address space AliasAnalysis
    implementation between the backends.
    
    Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

---
+++ llvm/include/llvm/Analysis/VMAliasAnalysis.h
@@ -0,0 +1,50 @@
+//===-- VMAliasAnalysis.h - VM alias analysis -------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This is the address space based alias analysis pass, that is shared between
+// EraVM and EVM backends.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_ANALYSIS_VMALIASANALYSIS_H
+#define LLVM_ANALYSIS_VMALIASANALYSIS_H
+
+#include "llvm/Analysis/AliasAnalysis.h"
+
+namespace llvm {
+
+class DataLayout;
+class MemoryLocation;
+
+/// A simple AA result that uses address space info to answer queries.
+class VMAAResult : public AAResultBase {
+  const DataLayout &DL;
+  SmallDenseSet<unsigned> StorageAS;
+  SmallDenseSet<unsigned> HeapAS;
+  unsigned MaxAS;
+
+public:
+  explicit VMAAResult(const DataLayout &DL,
+                      const SmallDenseSet<unsigned> &StorageAS,
+                      const SmallDenseSet<unsigned> &HeapAS, unsigned MaxAS)
+      : DL(DL), StorageAS(StorageAS), HeapAS(HeapAS), MaxAS(MaxAS) {}
+
+  /// Handle invalidation events from the new pass manager.
+  ///
+  /// By definition, this result is stateless and so remains valid.
+  bool invalidate(Function &, const PreservedAnalyses &,
+                  FunctionAnalysisManager::Invalidator &) {
+    return false;
+  }
+
+  AliasResult alias(const MemoryLocation &LocA, const MemoryLocation &LocB,
+                    AAQueryInfo &AAQI, const Instruction *I);
+};
+} // end namespace llvm
+
+#endif // LLVM_ANALYSIS_VMALIASANALYSIS_H
