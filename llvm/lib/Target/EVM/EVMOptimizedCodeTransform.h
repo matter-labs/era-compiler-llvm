90ceec5933387a8cba8047a88a7bf0243ca805a5
Vladimir Radosavljevic <vr@matterlabs.dev>
2024-12-13 15:46:01 +0100

    [EVM] Refactor EVMOptimizedCodeTransform
    Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

--- llvm/lib/Target/EVM/EVMOptimizedCodeTransform.h
+++ llvm/lib/Target/EVM/EVMOptimizedCodeTransform.h
@@ -15,12 +15,7 @@
 #define LLVM_LIB_TARGET_EVM_EVMOPTIMIZEDCODETRANSFORM_H
 
 #include "EVMAssembly.h"
-#include "EVMControlFlowGraph.h"
 #include "EVMStackLayoutGenerator.h"
-#include "llvm/CodeGen/LiveIntervals.h"
-#include "llvm/CodeGen/MachineLoopInfo.h"
-
-#include <stack>
 
 namespace llvm {
 
@@ -29,61 +24,38 @@ class MCSymbol;
 
 class EVMOptimizedCodeTransform {
 public:
-  static void run(EVMAssembly &Assembly, MachineFunction &MF,
-                  const LiveIntervals &LIS, MachineLoopInfo *MLI);
+  EVMOptimizedCodeTransform(EVMAssembly &Assembly, const StackLayout &Layout,
+                            MachineFunction &MF)
+      : Assembly(Assembly), Layout(Layout), MF(MF) {}
 
-  /// Generate code for the function call \p Call. Only public for using with
-  /// std::visit.
-  void operator()(CFG::FunctionCall const &Call);
-  /// Generate code for the builtin call \p Call. Only public for using with
-  /// std::visit.
-  void operator()(CFG::BuiltinCall const &Call);
-  /// Generate code for the assignment \p Assignment. Only public for using
-  /// with std::visit.
-  void operator()(CFG::Assignment const &Assignment);
+  /// Stackify instructions, starting from the \p EntryBB.
+  void run(CFG::BasicBlock &EntryBB);
 
 private:
-  EVMOptimizedCodeTransform(EVMAssembly &Assembly, const CFG &Cfg,
-                            const StackLayout &Layout, MachineFunction &MF);
+  EVMAssembly &Assembly;
+  const StackLayout &Layout;
+  const MachineFunction &MF;
+  Stack CurrentStack;
+  DenseMap<const MachineInstr *, MCSymbol *> CallToReturnMCSymbol;
 
   /// Checks if it's valid to transition from \p SourceStack to \p
   /// TargetStack, that is \p SourceStack matches each slot in \p
   /// TargetStack that is not a JunkSlot exactly.
-  static bool AreLayoutsCompatible(Stack const &SourceStack,
-                                   Stack const &TargetStack);
+  bool areLayoutsCompatible(const Stack &SourceStack, const Stack &TargetStack);
 
-  /// Shuffles CurrentStack to the desired \p TargetStack while emitting the
-  /// shuffling code to Assembly.
-  void createStackLayout(Stack TargetStack);
+  /// Shuffles CurrentStack to the desired \p TargetStack.
+  void createStackLayout(const Stack &TargetStack);
 
   /// Creates the Op.Input stack layout from the 'CurrentStack' taking into
   /// account commutative property of the operation.
-  void createOperationEntryLayout(const CFG::Operation &Op);
+  void createOperationLayout(const CFG::Operation &Op);
 
-  /// Generate code for the given block \p Block.
-  /// Expects the current stack layout 'CurrentStack' to be a stack layout that
-  /// is compatible with the entry layout expected by the block. Recursively
-  /// generates code for blocks that are jumped to. The last emitted assembly
-  /// instruction is always an unconditional jump or terminating. Always exits
-  /// with an empty stack layout.
-  void operator()(CFG::BasicBlock const &Block);
-
-  /// Generate code for the given function.
-  /// Resets CurrentStack.
-  void operator()();
-
-  EVMAssembly &Assembly;
-  StackLayout const &Layout;
-  const MachineFunction &MF;
-  const CFG::BasicBlock &EntryBB;
-  const std::vector<StackSlot> &Parameters;
-
-  Stack CurrentStack;
-  DenseMap<const MachineInstr *, MCSymbol *> CallToReturnMCSymbol;
-  DenseMap<const CFG::BasicBlock *, MCSymbol *> BlockLabels;
-  /// Set of blocks already generated. If any of the contained blocks is ever
-  /// jumped to, BlockLabels should contain a jump label for it.
-  std::set<CFG::BasicBlock const *> GeneratedBlocks;
+  /// Generate code for the function call \p Call.
+  void visitCall(const CFG::FunctionCall &Call);
+  /// Generate code for the builtin call \p Call.
+  void visitInst(const CFG::BuiltinCall &Call);
+  /// Generate code for the assignment \p Assignment.
+  void visitAssign(const CFG::Assignment &Assignment);
 };
 
 } // namespace llvm
