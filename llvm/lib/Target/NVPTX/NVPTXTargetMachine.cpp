9e9b9f21f57e66bc75f4258ae7187382ceb38767
Chengjun <chengjunp@Nvidia.com>
2025-05-07 15:25:48 -0700

    [AA] Move Target Specific AA before BasicAA (#125965)
    In this change, NVPTX AA is moved before Basic AA to potentially improve
    compile time. Additionally, it introduces a flag in the
    `ExternalAAWrapper` that allows other backends to run their
    target-specific AA passes before Basic AA, if desired.
    
    The change works for both New Pass Manager and Legacy Pass Manager.
    
    Original implementation by Princeton Ferro <pferro@nvidia.com>

--- llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
+++ llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
@@ -221,7 +221,7 @@ MachineFunctionInfo *NVPTXTargetMachine::createMachineFunctionInfo(
                                                                     F, STI);
 }
 
-void NVPTXTargetMachine::registerDefaultAliasAnalyses(AAManager &AAM) {
+void NVPTXTargetMachine::registerEarlyDefaultAliasAnalyses(AAManager &AAM) {
   AAM.registerFunctionAnalysis<NVPTXAA>();
 }
 
@@ -317,10 +317,7 @@ void NVPTXPassConfig::addIRPasses() {
   disablePass(&ShrinkWrapID);
 
   addPass(createNVPTXAAWrapperPass());
-  addPass(createExternalAAWrapperPass([](Pass &P, Function &, AAResults &AAR) {
-    if (auto *WrapperPass = P.getAnalysisIfAvailable<NVPTXAAWrapperPass>())
-      AAR.addAAResult(WrapperPass->getResult());
-  }));
+  addPass(createNVPTXExternalAAWrapperPass());
 
   // NVVMReflectPass is added in addEarlyAsPossiblePasses, so hopefully running
   // it here does nothing.  But since we need it for correctness when lowering
