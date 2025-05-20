6bb5f2501a3d2c9f04dad816de09542d57e1b17e
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2025-05-21 01:13:18 +0200

    [EVM] Improve code generation for conditional jumps
    The patch introduces a few SelectionDAG patterns to improve codegen for the
    `br (brcond (setcc))` case. Additionally, due to the late expansion of
    JUMP_UNLESS, the patch introduces a peephole pass that optimizes JUMPI
    predicates:
    
    * `ISZERO ISZERO` is folded to nothing
    * `EQ ISZERO` is folded to `SUB`
    * `SUB ISZERO` is folded to `EQ`

--- llvm/lib/Target/EVM/EVMTargetMachine.cpp
+++ llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -44,10 +44,14 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEVMTarget() {
   // Register the target.
   const RegisterTargetMachine<EVMTargetMachine> X(getTheEVMTarget());
   auto &PR = *PassRegistry::getPassRegistry();
-  initializeEVMCodegenPreparePass(PR);
+  initializeEVMAAWrapperPassPass(PR);
   initializeEVMAllocaHoistingPass(PR);
+  initializeEVMBPStackificationPass(PR);
+  initializeEVMCodegenPreparePass(PR);
+  initializeEVMExternalAAWrapperPass(PR);
   initializeEVMLinkRuntimePass(PR);
   initializeEVMLowerIntrinsicsPass(PR);
+  initializeEVMLowerJumpUnlessPass(PR);
   initializeEVMOptimizeLiveIntervalsPass(PR);
   initializeEVMSingleUseExpressionPass(PR);
   initializeEVMSplitCriticalEdgesPass(PR);
